ea6368b43e4b4bb54cbb80daa68e4f8fbdfda555
Wynn <wynnforthework@gmail.com>
2024-08-17 14:31:36 +0800

    fix(Enemy):add enemy spawner

--- Assets/Scripts/Enemy/EnemyStats.cs
+++ Assets/Scripts/Enemy/EnemyStats.cs
@@ -3,6 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 using MoreMountains.TopDownEngine;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
 public class EnemyStats : MonoBehaviour
 {
@@ -12,11 +13,27 @@ public class EnemyStats : MonoBehaviour
     public float currentHealth { get; private set; }
     public float currentDamage { get; private set; }
 
+    public float despawnDistance = 20f;
+    private Transform player;
+
     void Awake()
     {
         currentMoveSpeed = enemyData.MoveSpeed;
         currentHealth = enemyData.MaxHealth;
         currentDamage = enemyData.Damage;
+    }
+
+    void Start()
+    {
+        player = FindObjectOfType<PlayerStats>().transform;
+    }
+
+    void Update()
+    {
+        if (Vector2.Distance(transform.position,player.position)>= despawnDistance)
+        {
+            ReturnEnemy();
+        }
     }
 
     public void TakeDamage(float dmg)
@@ -41,5 +58,11 @@ public class EnemyStats : MonoBehaviour
             PlayerStats player = col.gameObject.GetComponent<PlayerStats>();
             player.TakeDamage(currentDamage);
         }
+    }
+
+    void ReturnEnemy()
+    {
+        EnemySpawner es = FindObjectOfType<EnemySpawner>();
+        transform.position = player.position + es.relativeSpawnPoints[Random.Range(0, es.relativeSpawnPoints.Count)].position;
     }
 }
